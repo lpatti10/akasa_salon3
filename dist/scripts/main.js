9a5e4ddf3d2d02e45dd0909b84dd90b0eaf1f1f0
Laura Patti <lpatti10@gmail.com>
2014-07-05 13:07:12 -0400

    With JS version built successfully

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('The Iron Yard Rocks');
+//Hover state for spotlight features
+$(".tile").hover( function(){
+	  $(this).addClass("border");
+	  }, function() {
+	 	$(this).removeClass("border");
+});
+
+//Hero image blur on scroll
+(function() {
+  $(window).scroll(function() {
+    var scrollBlur;
+    scrollBlur = $(window).scrollTop() / 100;
+    return $(".blur").css("opacity", scrollBlur);
+  });
+
+//Commenting this out will omit blur on scroll
+}).call(this);
+
+
